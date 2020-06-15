90b611d5200ecc296d3db3fe4541bee2e6a8d7e9
Vinit Joshi <vnj2302tech@outlook.com>
2020-06-15 10:05:54 -0700

    2nd commit

--- Post GIT and R Integration.R
+++ Post GIT and R Integration.R
@@ -1,3 +1,5 @@
-#First Filr to learn GitHub and R Integration
+#First File to learn GitHub and R Integration
 
 print ("First file post creation of new GIT repository and cloning locally")
+
+print ("one new line added")
